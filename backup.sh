2f9e7d3540bc9108f92760c28be58ca68f192987
ignatiy <ignatii1994-23-10.ru@mail.ru>
2017-06-11 13:04:25 +0700

    Create backup.sh

--- backup.sh
+++ backup.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-NST=ftp-$(date +'%d-%b-%Y-%R')
+NST=filename-$(date +'%d-%b-%Y-%R')
 OF=$NST.tar
 OLD_IFS=$IFS
 IFS=$'\n'
 SRCD="/media/media/backup"
-TDTD
+TGTD="/home/backups"
+tar -cJf $TGTD$OF $SRCD
+curl -T $TGTD$OF --user username:password https://webdav.yandex.ru/
+STATUS=$?
+IFS=$OLD_IFS
+if [[ $STATUS != 0 ]]; then
+  rm $TGTD$OF 
+  echo "$(date +'%d-%b-%Y-%R%nBackup') $OF error" 
+else
+  echo "Backup complete!" 
+  rm $TGTD$OF
+fi
+exit
