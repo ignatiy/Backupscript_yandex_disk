4c8c4a46b36a5523493f2b90dbc4881777e53043
IgnatiyOS <ignatii1994-23-10.ru@mail.ru>
2017-09-19 18:38:33 +0700

    update from commit backup.sh
    add coment for scripts

--- backup.sh
+++ backup.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-NST=filename-$(date +'%d-%b-%Y-%R')
-OF=$NST.tar
-OLD_IFS=$IFS
+NST=filename-$(date +'%d-%b-%Y-%R') # задаем имя нашего файла с датой и временем
+OF=$NST.tar # такой вид примет наш файл на выходе
+OLD_IFS=$IFS # объявляем переменные
 IFS=$'\n'
-SRCD="/media/media/backup"
-TGTD="/home/backups"
-tar -cJf $TGTD$OF $SRCD
-curl -T $TGTD$OF --user username:password https://webdav.yandex.ru/
-STATUS=$?
+SRCD="/media/media/backup" # путь куда бекапить
+TGTD="/home/backups" # путь что бекапить
+tar -cJf $TGTD$OF $SRCD # задаем параметры архивирования
+curl -T $TGTD$OF --user username:password https://webdav.yandex.ru/ # отправляем бекап на яндекс диск
+STATUS=$? # проверяем статус бекапа
 IFS=$OLD_IFS
-if [[ $STATUS != 0 ]]; then
+if [[ $STATUS != 0 ]]; then # если ошибка то бекап не создаем и удаляем то что уже создано. при этом отправка на яндекс диск не осуществляется
   rm $TGTD$OF 
-  echo "$(date +'%d-%b-%Y-%R%nBackup') $OF error" 
-else
-  echo "Backup complete!" 
+  echo "$(date +'%d-%b-%Y-%R%nBackup') $OF error" # выводим в терминал сообщение об ошибке
+else # если всё хорошо бекапим, сохраняем на диск и удаляем локальный бекап
+  echo "Backup complete!" # выводим в терминал сообщение об успешном выполнении задачи
   rm $TGTD$OF
 fi
-exit
+exit # выходим из программы
